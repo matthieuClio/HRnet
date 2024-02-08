41f53e0f78b96b69c065cb204c6d0753a54f996a
Matthieu <matthieu.clio@gmail.com>
2024-02-08 16:36:21 +0100

    Feat : make a design for the user information presentation.

--- src/page/EmployeeList.tsx
+++ src/page/EmployeeList.tsx
@@ -1,5 +1,57 @@
+// React router
+import { Link } from "react-router-dom";
+
+// Style
+import './employeeList.scss';
+
 export default function EmployeeList () {
     return (
-        'ok'
+        <section className="employee-list">
+            <h1>
+                Current Employees (tableau)
+            </h1>
+
+            <Link to="/" className="employee-list__link">
+                Home
+            </Link>
+
+            <section className="employee-list__user">
+                <h2 className="employee-list__user__title">
+                    Title
+                </h2>
+
+                <div className="employee-list__user__info-user">
+                    Info User
+                    <br />
+                    <br />
+                    Frist name : Mat
+                    <br />
+                    Name : c
+                    <br />
+                    Etc...
+                </div>
+
+                <div className="employee-list__user__info-user">
+                    Info User
+                    <br />
+                    <br />
+                    Frist name : Mat
+                    <br />
+                    Name : c
+                    <br />
+                    Etc...
+                </div>
+                <div className="employee-list__user__info-user">
+                    Info User
+                    <br />
+                    <br />
+                    Frist name : Mat
+                    <br />
+                    Name : c
+                    <br />
+                    Etc...
+                </div>
+            </section>
+        </section>
     );
 }
